fa34b0b2725332f70cb239898f73e698feae3094
sara-jac <160785788+sara-jac@users.noreply.github.com>
2024-10-22 12:48:32 +0200

    Added basic search engine functionality

---
+++ src/index.js
@@ -0,0 +1,9 @@
+function searchCity(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input"); 
+  let currentCityElement = document.querySelector("#current-city");
+  currentCityElement.innerHTML = searchInput.value;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", searchCity);
