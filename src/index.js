08221c6e3a1d81e364c9938b6f8d6a5249ea8fe3
sara-jac <160785788+sara-jac@users.noreply.github.com>
2024-10-22 16:35:56 +0200

    Fetched more weather data

--- src/index.js
+++ src/index.js
@@ -1,10 +1,41 @@
 function getWeather(response) {
   let temperatureElement = document.querySelector("#temperature-value");
   let temperature = response.data.temperature.current;
-  let currentCityElement = document.querySelector("#current-city");
+  let cityElement = document.querySelector("#current-city");
+  let descriptionElement = document.querySelector("#temperature-description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#current-time");
+  let date = new Date(response.data.time * 1000); 
 
-  currentCityElement.innerHTML = response.data.city;
+  cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
   temperatureElement.innerHTML = Math.round(temperature);   
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+}
+
+function formatDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = 
+  [
+    "Sunday",
+    "Monday", 
+    "Tuesday", 
+    "Wednesday", 
+    "Thursday", 
+    "Friday", 
+    "Saturday",
+  ]
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`
+  }
+
+  return `${day}, ${hours}:${minutes}`
 }
 
 
